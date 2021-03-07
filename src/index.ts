0a98c79ba8ba54c24b89b60994dec3d332c2a67a
Alan Terriaga <alanst32@gmail.com>
2021-03-07 16:13:29 +1100

    comit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 
 import express from 'express';
-import path from 'path';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import router from './routes/index.routes';
 
@@ -31,17 +28,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('LOG: database connected'));
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// set engine for rendering
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../../client/src'));
-
-// app.get('/', (req, res) => {
-//     res.status(200).send('Hello World!');
-// });
 
 app.use('/', router);
 
